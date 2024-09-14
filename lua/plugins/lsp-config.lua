9685a7c010de6e6932359be185c7450bbcee51a5
gc4d <guilhermeacadorin@gmail.com>
2024-09-13 22:22:42 -0300

    Updating configs

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "clangd",
+          "omnisharp"
+        }
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({})
+      lspconfig.omnisharp.setup({})
+      lpsconfig.clangd.setup({})
+    end
+  }
+}
