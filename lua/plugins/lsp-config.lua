7db1bba04ddac26a6af6924bf813d624a093cbca
gc4d <guilhermeacadorin@gmail.com>
2024-09-14 23:42:54 -0300

    Adding lsp config

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -23,7 +23,12 @@ return {
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
       lspconfig.omnisharp.setup({})
-      lpsconfig.clangd.setup({})
+      lspconfig.clangd.setup({})
+
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
+
     end
   }
 }
